54a65fd5b2b7f6009c6b9c4a31d2253932f32775
TheRandomLabs <therandomlabsinc@gmail.com>
2018-11-25 13:47:27 +1100

    Fix crash

--- src/main/java/com/therandomlabs/randompatches/core/patch/WorldServerPatch.java
+++ src/main/java/com/therandomlabs/randompatches/core/patch/WorldServerPatch.java
@@ -6,27 +6,43 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 
 public final class WorldServerPatch extends Patch {
 	@Override
 	public void apply(ClassNode node) {
 		final MethodNode method = findMethod(node, "<init>");
-		MethodInsnNode createTeleporter = null;
+
+		TypeInsnNode createTeleporter = null;
+		MethodInsnNode initTeleporter = null;
 
 		for(int i = 0; i < method.instructions.size(); i++) {
 			final AbstractInsnNode instruction = method.instructions.get(i);
 
-			if(instruction.getOpcode() == Opcodes.INVOKESPECIAL) {
-				createTeleporter = (MethodInsnNode) instruction;
+			if(createTeleporter == null) {
+				if(instruction.getOpcode() == Opcodes.NEW) {
+					createTeleporter = (TypeInsnNode) instruction;
 
-				if("net/minecraft/world/Teleporter".equals(createTeleporter.owner)) {
+					if(!"net/minecraft/world/Teleporter".equals(createTeleporter.desc)) {
+						createTeleporter = null;
+					}
+
+					continue;
+				}
+			}
+
+			if(instruction.getOpcode() == Opcodes.INVOKESPECIAL) {
+				initTeleporter = (MethodInsnNode) instruction;
+
+				if("net/minecraft/world/Teleporter".equals(initTeleporter.owner)) {
 					break;
 				}
 
-				createTeleporter = null;
+				initTeleporter = null;
 			}
 		}
 
-		createTeleporter.owner = "com/therandomlabs/verticalendportals/util/VEPTeleporter";
+		createTeleporter.desc = "com/therandomlabs/verticalendportals/util/VEPTeleporter";
+		initTeleporter.owner = "com/therandomlabs/verticalendportals/util/VEPTeleporter";
 	}
 }
