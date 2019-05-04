d56d6a7434eb81b6f15fd58f363dad42ba37e99e
TheRandomLabs <therandomlabsinc@gmail.com>
2019-05-04 19:11:12 +1000

    ServerRecipePlacer

--- src/main/java/com/therandomlabs/randompatches/patch/ServerRecipePlacerPatch.java
+++ src/main/java/com/therandomlabs/randompatches/patch/ServerRecipePlacerPatch.java
@@ -1,38 +1,10 @@
 package com.therandomlabs.randompatches.patch;
 
-import com.therandomlabs.randompatches.Patch;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.item.ItemStack;
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
-import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
-import org.objectweb.asm.tree.MethodNode;
 
-public final class ServerRecipePlacerPatch extends Patch {
-	@Override
-	public boolean apply(ClassNode node) {
-		final MethodNode method = findMethod(node, "consumeIngredient", "func_194325_a");
-
-		MethodInsnNode findSlotMatchingUnusedItem = null;
-
-		for(int i = 0; i < method.instructions.size(); i++) {
-			final AbstractInsnNode instruction = method.instructions.get(i);
-
-			if(instruction.getOpcode() == Opcodes.INVOKEVIRTUAL) {
-				findSlotMatchingUnusedItem = (MethodInsnNode) instruction;
-				break;
-			}
-		}
-
-		findSlotMatchingUnusedItem.setOpcode(Opcodes.INVOKESTATIC);
-		findSlotMatchingUnusedItem.owner = getName(ServerRecipePlacerPatch.class);
-		findSlotMatchingUnusedItem.name = "findSlotMatchingUnusedItem";
-		findSlotMatchingUnusedItem.desc =
-				"(Lnet/minecraft/entity/player/InventoryPlayer;Lnet/minecraft/item/ItemStack;)I";
-
-		return true;
-	}
+public final class ServerRecipePlacerPatch {
+	private ServerRecipePlacerPatch() {}
 
 	public static int findSlotMatchingUnusedItem(InventoryPlayer inventory, ItemStack toMatch) {
 		for(int i = 0; i < inventory.mainInventory.size(); i++) {
---
+++ src/main/resources/patches/ServerRecipePlacer.js
@@ -0,0 +1,62 @@
+var Opcodes = Java.type("org.objectweb.asm.Opcodes");
+
+function log(message) {
+	print("[RandomPatches ServerRecipePlacer Transformer]: " + message);
+}
+
+function patch(method, name, srgName, patchFunction) {
+	if(method.name != name && method.name != srgName) {
+		return false;
+	}
+
+	log("Patching method: " + name + " (" + method.name + ")");
+	patchFunction(method.instructions);
+	return true;
+}
+
+function initializeCoreMod() {
+	return {
+		"RandomPatches ServerRecipePlacer Transformer": {
+			"target": {
+				"type": "CLASS",
+				"name": "net.minecraft.item.crafting.ServerRecipePlacer"
+			},
+			"transformer": function(classNode) {
+				log("Transforming class: " + classNode.name);
+
+				var methods = classNode.methods;
+
+				for(var i in methods) {
+					if(patch(
+							methods[i], "consumeIngredient", "func_194325_a",
+							patchConsumeIngredient
+					)) {
+						break;
+					}
+				}
+
+				return classNode;
+			}
+		}
+	};
+}
+
+function patchConsumeIngredient(instructions) {
+	var findSlotMatchingUnusedItem;
+
+	for(var i = 0; i < instructions.size(); i++) {
+		var instruction = instructions.get(i);
+
+		if(instruction.getOpcode() == Opcodes.INVOKEVIRTUAL) {
+			findSlotMatchingUnusedItem = instruction;
+			break;
+		}
+	}
+
+	findSlotMatchingUnusedItem.setOpcode(Opcodes.INVOKESTATIC);
+	findSlotMatchingUnusedItem.owner =
+			"com/therandomlabs/randompatches/patch/ServerRecipePlacerPatch";
+	findSlotMatchingUnusedItem.name = "findSlotMatchingUnusedItem";
+	findSlotMatchingUnusedItem.desc =
+			"(Lnet/minecraft/entity/player/InventoryPlayer;Lnet/minecraft/item/ItemStack;)I";
+}
