cecf9e76bae92b0c96bd188aa12787a0d0659ae2
TheRandomLabs <therandomlabsinc@gmail.com>
2019-07-05 13:46:44 +1000

    Comment

--- src/main/java/com/therandomlabs/randompatches/patch/client/RenderPlayerPatch.java
+++ src/main/java/com/therandomlabs/randompatches/patch/client/RenderPlayerPatch.java
@@ -30,7 +30,7 @@ public final class RenderPlayerPatch extends Patch {
 	}
 
 	//In RenderPlayer#applyRotations, Math#acos is sometimes called with a value larger than 1.0,
-	//making the rotation angle NaN and making the player model disappear
+	//making the rotation angle NaN and causing the player model to disappear
 	//This issue is noticeable when flying with elytra in a straight line in third-person mode
 	public static double acos(double a) {
 		return Math.acos(Math.min(a, 1.0));
