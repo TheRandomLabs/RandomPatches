19af55619a2093ef8e9348e4373d374956785431
TheRandomLabs <therandomlabsinc@gmail.com>
2020-07-17 19:09:04 +1000

    Fixes #113

--- src/main/java/com/therandomlabs/randompatches/RandomPatches.java
+++ src/main/java/com/therandomlabs/randompatches/RandomPatches.java
@@ -4,9 +4,9 @@ import com.therandomlabs.utils.config.ConfigManager;
 import com.therandomlabs.utils.forge.config.ConfigReloadCommand;
 import com.therandomlabs.utils.forge.config.ForgeConfig;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.RegisterCommandsEvent;
 import net.minecraftforge.fml.DistExecutor;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -21,14 +21,14 @@ public final class RandomPatches {
 	public RandomPatches() {
 		ForgeConfig.initialize();
 		ConfigManager.register(RPConfig.class);
-		MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
+		MinecraftForge.EVENT_BUS.addListener(this::registerCommands);
 		PROXY.init();
 	}
 
-	private void serverStarting(FMLServerStartingEvent event) {
+	private void registerCommands(RegisterCommandsEvent event) {
 		if (RPConfig.Misc.rpreload) {
 			ConfigReloadCommand.server(
-					event.getCommandDispatcher(), "rpreload", "rpreloadclient", RPConfig.class,
+					event.getDispatcher(), "rpreload", "rpreloadclient", RPConfig.class,
 					"RandomPatches configuration reloaded!",
 					(phase, source) -> RPConfig.Window.setWindowSettings =
 							phase == ConfigReloadCommand.ReloadPhase.POST
